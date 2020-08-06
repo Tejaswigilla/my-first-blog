46abe3afe3bd9d153645395abbb1f7d07910b508
Tejaswi <tejaswigilla@gmail.com>
2020-08-06 13:28:35 +0530

    done2

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.utils import timezone
+
 
 # Create your views here.
 def post_list(request):
